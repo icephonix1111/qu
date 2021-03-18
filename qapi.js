4ef5f13e06960743e3fa9a3dc07fdb87930a17bb
icephonix1111 <72299480+icephonix1111@users.noreply.github.com>
2021-03-18 10:32:31 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"604067fa1a979b001ff33a50",
+				"604067fa1a979b001ff33a52"
+			]
+		},
+		"604067fa1a979b001ff33a50": {
+			"id": "604067fa1a979b001ff33a50",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"604067fa1a979b001ff33a52": {
+			"id": "604067fa1a979b001ff33a52",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
